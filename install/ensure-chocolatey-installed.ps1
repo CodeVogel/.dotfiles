bebb78aa2ba8217ae7ec1c831408abfd529218b4
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-20 20:13:40 +0200

    rn ReadHost to Read-Host

--- install/ensure-chocolatey-installed.ps1
+++ install/ensure-chocolatey-installed.ps1
@@ -4,10 +4,10 @@ $choc_installed = Get-Command -Name choco.exe -ErrorAction SilentlyContinue
 
 if (-Not $choc_installed)
 {
-   ReadHost "Chocolatey is not installed. Press Enter to try to install it."
+   Read-Host "Chocolatey is not installed. Press Enter to try to install it."
    & "//wsl$/Ubuntu/home/$(wsl whoami)/.dotfiles/install/install-chocolatey.ps1"
-   ReadHost "Chocolatey is now installed. Press Enter to continue."
+   Read-Host "Chocolatey is now installed. Press Enter to continue."
 }
 else {
-   ReadHost "Chocolatey is already installed. Press Enter to continue."
+   Read-Host "Chocolatey is already installed. Press Enter to continue."
 }
