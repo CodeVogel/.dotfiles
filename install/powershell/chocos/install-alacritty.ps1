bc0c69eaab70fa947be7d7b143ef81ecc716e31a
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-21 11:29:31 +0200

    fix if syntax

--- install/powershell/chocos/install-alacritty.ps1
+++ install/powershell/chocos/install-alacritty.ps1
@@ -6,12 +6,12 @@ $link_path = "$link_dir\alacritty.toml"
 $target_path = "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\alacritty\alacritty.toml"
 
 # Throw error if alacritty toml is missing
-if (-Not Test-Path $targetpath) {
+if (-Not (Test-Path $targetpath)) {
    Write-Output "Tried making a symlink $link_path to target $target_path but target does not exist."
 }
 
 # Create directory %APPDATA%\alacritty if it does not exist
-if (-Not Test-Path $link_dir) {
+if (-Not (Test-Path $link_dir)) {
    New-Item -ItemType Directory -Path $link_dir -Name "alacritty"
 }
 
