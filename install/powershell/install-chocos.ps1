8a955cbd831df0da7092e5a67f9751c1d0b81bb6
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-21 12:33:42 +0200

    add active.conf

--- install/powershell/install-chocos.ps1
+++ install/powershell/install-chocos.ps1
@@ -1,20 +1,33 @@
 Read-Host "Starting installation of tools through Chocolatey. Press Enter to continue."
 
-$chocos_dir = "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\install\powershell\chocos"
-$chocos = Get-ChildItem -Path $chocos_dir -Filter "*.ps1"
+$scriptsDir = "//wsl$/Ubuntu/home/$(wsl whoami)/.dotfiles/install/powershell/chocos"
+$activeConf = Join-Path -Path $scriptsDir -ChildPath "active.conf"
 
-foreach ($chocolate in $chocos) {
-   $name = $chocolate.Name
-   $path = Join-Path -Path $chocos_dir -ChildPath $name
-   if (Test-Path $path) {
-      Write-Host "Now installing $name"
-      & $path
-      Write-Host "Finished installing $name"
+# Check if the conf file exists
+if (!(Test-Path $activeConf)) {
+    Write-Host "Configuration file not found at $activeConf"
+    exit
+}
+
+# Read the script paths from the conf file and filter out comments
+$activeScripts = Get-Content $activeConf | Where-Object { $_ -notmatch "^#" }
+
+foreach ($scriptPath in $activeScripts) {
+   # Resolve relative paths
+   if ($scriptPath -match "^\.") {
+      $scriptPath = Resolve-Path (Join-Path -Path $scriptsDir -ChildPath $scriptPath)
+   }
+
+   if (Test-Path $scriptPath) {
+      Write-Host "Now installing $scriptPath"
+      & $scriptPath
+      Write-Host "Finished installing $scriptPath"
       RefreshEnv
    } else {
-      Write-Host "Did not find $name at $path"
+      Write-Host "Did not find script at $scriptPath"
       break
    }
 }
 
-Read-Host "All done with the chocos. Press Enter To Continue."
+Read-Host "All done with the chocos. Press Enter To Continue"
+
