0e40310ea4019fb3bf33383f8323a14b22efab89
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-21 16:55:05 +0200

    refactor to camelCase

--- install/powershell/chocos/install-alacritty.ps1
+++ install/powershell/chocos/install-alacritty.ps1
@@ -1,19 +1,19 @@
 # Install alacritty with choco
 choco install alacritty -y
 
-$link_dir = "$env:APPDATA\alacritty"
-$link_path = "$link_dir\alacritty.toml"
-$target_path = "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\alacritty\alacritty.toml"
+$linkDir = "$env:APPDATA\alacritty"
+$linkPath = "$linkDir\alacritty.toml"
+$targetPath = "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\alacritty\alacritty.toml"
 
 # Throw error if alacritty toml is missing
-if (-Not (Test-Path $target_path)) {
-   Write-Output "Tried making a symlink $link_path to target $target_path but target does not exist."
+if (-Not (Test-Path $targetPath)) {
+   Write-Output "Tried making a symlink $linkPath to target $targetPath but target does not exist."
 }
 
 # Create directory %APPDATA%\alacritty if it does not exist
-if (-Not (Test-Path $link_dir)) {
-   New-Item -ItemType Directory -Path $link_dir -Name "alacritty"
+if (-Not (Test-Path $linkDir)) {
+   New-Item -ItemType Directory -Path $env:APPDATA -Name "alacritty"
 }
 
 # Symlink alacritty config from the expected path on Windows into .dotfiles
-New-Item -ItemType SymbolicLink -Path $link_path -Target $target_path
+New-Item -ItemType SymbolicLink -Path $linkPath -Target $targetPath
