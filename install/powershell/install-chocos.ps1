5c51e63c430d54913980826537233be5f97f4ea9
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-21 12:16:21 +0200

    install all chocos from /install/powershell/chocos/

--- install/powershell/install-chocos.ps1
+++ install/powershell/install-chocos.ps1
@@ -1,20 +1,20 @@
 Read-Host "Starting installation of tools through Chocolatey. Press Enter to continue."
 
-$scripts = @(
-   "install-alacritty.ps1"
-)
+$chocos_dir = "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\install\powershell\chocos"
+$chocos = Get-ChildItem -Path $chocos_dir -Filter "*.ps1"
 
-foreach ($scriptname in $scripts) {
-   $path = "$env:WSLHOME_IN_WIN/.dotfiles/install/powershell/chocos/$scriptname"
+foreach ($chocolate in $chocos) {
+   $name = $chocolate.Name
+   $path = Join-Path -Path $chocos_dir -ChildPath $name
    if (Test-Path $path) {
-      Write-Host "Now installing $scriptname"
+      Write-Host "Now installing $name"
       & $path
-      Write-Host "Finished installing $scriptname"
+      Write-Host "Finished installing $name"
       RefreshEnv
    } else {
-      Write-Host "Did not find $scriptname at $path"
+      Write-Host "Did not find $name at $path"
       break
    }
 }
 
-Read-Host "All done with the chocos. Press Enter To Continue"
+Read-Host "All done with the chocos. Press Enter To Continue."
