77edb03bac9873fc16289441385ccbc72c1e5bb5
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-21 18:57:00 +0200

    refactor install files into function

--- install/bash/install-linux.sh
+++ install/bash/install-linux.sh
@@ -3,6 +3,33 @@
 # Update available packages
 sudo apt update
 
-source ~/.dotfiles/install/bash/install-apt-packages.sh
+# Define a function to read and source a configuration file
+read_and_source() {
+    INSTALL_TYPE=$1
+    CONF_FILE="~/.dotfiles/install/bash/$INSTALL_TYPE/active.conf"
 
-source ~/.dotfiles/install/bash/install-curl-packages.sh
+    # Check if the configuration file exists
+    if [ ! -f "$CONF_FILE" ]; then
+        echo "Configuration file not found: $CONF_FILE"
+        exit 1
+    fi
+
+    # Read the configuration file line by line
+    while IFS= read -r line
+    do
+        # Ignore lines starting with '#'
+        if [[ $line != \#* ]]; then
+            # Check if the path is relative
+            if [[ $line != /* ]]; then
+                # Convert the relative path to an absolute path
+                line="/home/$(whoami)/.dotfiles/install/bash/$INSTALL_TYPE/$line"
+            fi
+            source "$line"
+        fi
+    done < "$CONF_FILE"
+}
+
+# Call the function with different configuration files
+read_and_source curl
+read_and_source apt
+read_and_source brew
