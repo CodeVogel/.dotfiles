f0e97184112f77c883b6b9cc05e787744752551e
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-23 20:31:24 +0200

    add go install script

---
+++ install/bash/curl/install-go.sh
@@ -0,0 +1,15 @@
+# Download the latest version URL
+latest=$(curl -s https://go.dev/VERSION?m=text | head -n 1)
+latest_url="https://golang.org/dl/$latest.linux-amd64.tar.gz"
+
+# Remove existing Go installation
+rm -rf /usr/local/go
+
+# Download the latest Go tarball using curl
+curl -o go.tar.gz -L "$latest_url"
+
+# Extract the downloaded tarball to /usr/local
+tar -C /usr/local -xzf go.tar.gz
+
+# Remove the downloaded tarball
+rm go.tar.gz
