062ed42afaf6d5a14d7fc063080d5876f9329bd6
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-23 16:10:37 +0200

    add sudo to chsh

--- install/bash/apt/install-zsh.sh
+++ install/bash/apt/install-zsh.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-# DEPS: 
-# apt update
-# stow
+# DEPS: stow
 
 sudo apt install zsh -y
 
 # Change default shell to zsh
-chsh -s $(which zsh) 
+me=$(whoami)
+sudo chsh -s $(which zsh) $me
+
 # Set dotfiles location for zsh
 export ZDOTDIR=~/.dotfiles/zsh
 
