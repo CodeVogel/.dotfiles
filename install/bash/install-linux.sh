eaac80c1e24081300202a1966f199cbf66dbb686
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-23 20:31:44 +0200

    formatting

--- install/bash/install-linux.sh
+++ install/bash/install-linux.sh
@@ -9,20 +9,19 @@ CONF_FILE="$INSTALL_DIR/active.conf"
 
 # Check if the configuration file exists
 if [ ! -f "$CONF_FILE" ]; then
-   echo "Configuration file not found: $CONF_FILE"
-   exit 1
+  echo "Configuration file not found: $CONF_FILE"
+  exit 1
 fi
 
 # Read the configuration file line by line
-while IFS= read -r line
-do
-   # Ignore lines starting with '#' and empty lines
-   if [[ $line != \#* ]] && [ -n "$line" ]; then
-      # If path starts with './', replace it with the path to the install directory 
-      if [[ $line == ./* ]]; then
-         line="$INSTALL_DIR/${line:2}"
-      fi
-      # source the stripped path
-      source $line | xargs
-   fi
-done < "$CONF_FILE"
+while IFS= read -r line; do
+  # Ignore lines starting with '#' and empty lines
+  if [[ $line != \#* ]] && [ -n "$line" ]; then
+    # If path starts with './', replace it with the path to the install directory
+    if [[ $line == ./* ]]; then
+      line="$INSTALL_DIR/${line:2}"
+    fi
+    # source the stripped path
+    source $line | xargs
+  fi
+done <"$CONF_FILE"
