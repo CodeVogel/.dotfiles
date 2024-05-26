2a9eed31a684d5b24758fa4b713f45f5bff5f358
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-26 17:09:02 +0200

    use symlink script

--- install/powershell/choco/install-wezterm.ps1
+++ install/powershell/choco/install-wezterm.ps1
@@ -1,4 +1,4 @@
 # Install wezterm with choco
 choco install wezterm -y
-& "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\install\powershell\make-symlink.ps1" -linkPath "$HOME/.wezterm.lua" -targetPath "\\wsl$\Ubuntu\home\$(whoami)\.dotfiles\wezterm\.wezterm.lua"
+& "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\install\powershell\make-symlink.ps1" -linkPath "$HOME/.wezterm.lua" -targetPath "\\wsl$\Ubuntu\home\$(wsl whoami)\.dotfiles\wezterm\.wezterm.lua"
 
