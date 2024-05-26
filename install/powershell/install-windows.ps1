0b1c21d7f19f288a895b30cde93b9ec484d3c6a9
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-27 00:05:05 +0200

    add chocolatey install

--- install/powershell/install-windows.ps1
+++ install/powershell/install-windows.ps1
@@ -4,5 +4,4 @@ $WSLHOME = "\\wsl$\Ubuntu\home\$(wsl whoami)"
 & $WSLHOME\.dotfiles\install\powershell\make-symlink.ps1 -linkPath $PSHOME\Profile.ps1 -targetPath $WSLHOME\.dotfiles\powershell\Profile.ps1
 
 # Install chocolatey
-PowerShell -Command "& $WSLHOME\.dotfiles\install\powershell\install-chocolatey.ps1"
-PowerShell -Command "& $WSLHOME\.dotfiles\install\powershell\install-chocos.ps1"
+Start-Process -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $WSLHOME\.dotfiles\install\powershell\install-chocolatey.ps1" -Verb RunAs
