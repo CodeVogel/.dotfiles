6fde4f63d858a1969859c600c828a583ac770e24
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-26 22:54:15 +0200

    update chocolatey install

--- install/powershell/install-chocolatey.ps1
+++ install/powershell/install-chocolatey.ps1
@@ -1,11 +1,22 @@
 # Run this in an administrative shell
 # Follows the installation instructions from https://chocolatey.org/install#individual 
 
-# Get-ExecutionPolicy may not be Restricted
+# Get-ExecutionPolicy must not be Restricted
 if ($(Get-ExecutionPolicy) -eq "Restricted")
 {
-   Set-ExecutionPolicy AllSigned
+   $response = Read-Host -Prompt "ExecutionPolicy is Restricted. Set the execution policy to AllSigned? (y/n)"
+   if ($response -eq "y")
+   {
+      Set-ExecutionPolicy AllSigned
+   }
+   else
+   {
+      Write-Host "Exiting script. ExecutionPolicy must be AllSigned or Unrestricted to install Chocolatey."
+      exit 1
+   }
 }
 
 # Install chocolatey through the shell script at https://community.chocolatey.org/install.ps1
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+Set-ExecutionPolicy Bypass -Scope Process -Force
+[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
