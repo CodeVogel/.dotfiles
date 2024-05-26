78960850e3867bff0eadc5bda3f0fb05297c1c18
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-27 00:21:53 +0200

    wait-process

--- install/powershell/install-windows.ps1
+++ install/powershell/install-windows.ps1
@@ -4,5 +4,6 @@ $WSLHOME = "\\wsl$\Ubuntu\home\$(wsl whoami)"
 & $WSLHOME\.dotfiles\install\powershell\make-symlink.ps1 -linkPath $PSHOME\Profile.ps1 -targetPath $WSLHOME\.dotfiles\powershell\Profile.ps1
 
 # Install chocolatey
-Start-Process -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $WSLHOME\.dotfiles\install\powershell\install-chocolatey.ps1 -Wait" -Verb RunAs
-Start-Process -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $WSLHOME\.dotfiles\install\powershell\install-chocos.ps1 -Wait" -Verb RunAs
+
+Start-Process -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $WSLHOME\.dotfiles\install\powershell\install-chocolatey.ps1 -Wait" -Verb RunAs | Wait-Process
+Start-Process -FilePath powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File $WSLHOME\.dotfiles\install\powershell\install-chocos.ps1 -Wait" -Verb RunAs | Wait-Process
