09eb6da8f5eb597d978bc8281c476da242c3555d
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-26 23:12:21 +0200

    add message

--- install/powershell/install-windows.ps1
+++ install/powershell/install-windows.ps1
@@ -1,7 +1,7 @@
+Read-Host -Prompt "Installing Windows Software. Press Enter to continue"
+
 $WSLHOME = "\\wsl$\Ubuntu\home\$(wsl whoami)"
 & $WSLHOME\.dotfiles\install\powershell\make-symlink.ps1 -linkPath $PSHOME\Profile.ps1 -targetPath $WSLHOME\.dotfiles\powershell\Profile.ps1
-
-
 
 # Install chocolatey
 PowerShell -Command "& $WSLHOME\.dotfiles\install\powershell\install-chocolatey.ps1"
