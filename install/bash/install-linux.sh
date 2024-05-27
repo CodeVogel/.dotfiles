971bf080febd5cd34c5305f2f63eb050052032c5
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-27 12:03:54 +0200

    source active files

--- install/bash/install-linux.sh
+++ install/bash/install-linux.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-# Update available packages
-sudo apt update
-
-source ~/.dotfiles/install/bash/dependency-check.sh
-
 # Check if installation order is correct
+source ~/.dotfiles/install/bash/dependency-check.sh
 install_dir="/home/$(whoami)/.dotfiles/install/bash"
 files_from_config=$(get_files_from_config $install_dir)
 
@@ -14,3 +10,16 @@ if ! check_install_order $files_from_config; then
   echo "Installation order is incorrect; stopping now."
   exit 1
 fi
+
+# Update available packages
+sudo apt update
+
+# Install packages
+for file in $files_from_config; do
+  # if file starts with ./, then it is a relative path
+  if [[ $file == "./"* ]]; then
+    file="$install_dir/$file"
+  fi
+  echo "Installing $file..."
+  source $file
+done
