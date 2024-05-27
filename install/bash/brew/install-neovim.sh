6a969002b54be116e0385d0710a83287af45b6aa
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-27 16:34:16 +0200

    make brew installs apt installs

--- install/bash/brew/install-neovim.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# deps: homebrew, stow
-brew install neovim
-stow -d $HOME/.dotfiles/ -t $HOME -S neovim
--- install/bash/brew/install-tmux.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# deps: homebrew, stow
-brew install tmux
-stow -d $HOME/.dotfiles/ -t $HOME -S tmux
