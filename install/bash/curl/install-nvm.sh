7f30efc5baa927beccc7569dea5faf6dff1a11bc
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-27 14:36:16 +0200

    export nvm

--- install/bash/curl/install-nvm.sh
+++ install/bash/curl/install-nvm.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc # adds nvm to the PATH
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
