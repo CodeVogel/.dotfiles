577b110623cf1ba4fce49880d4348f54080e2536
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-30 11:45:06 +0200

    add info message

--- install/bash/apt/install-tmux.sh
+++ install/bash/apt/install-tmux.sh
@@ -5,3 +5,5 @@ stow -d $HOME/.dotfiles/ -t $HOME -S tmux
 
 # install TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+echo 'ℹ️ TMUX is installed, but do not forget to install the tmux plugins! To do this, run tmux and press <leader> + I'
