827b64c0b3b1afb62d5637ced4da74135d860aba
Kamiel de Visser <kamieldevisser@gmail.com>
2024-05-30 14:52:53 +0200

    offer to re-install TPM

--- install/bash/apt/install-tmux.sh
+++ install/bash/apt/install-tmux.sh
@@ -3,6 +3,18 @@
 sudo apt-get install tmux -y
 stow -d $HOME/.dotfiles/ -t $HOME -S tmux
 
+# check if TPM is installed
+if [ -d ~/.tmux/plugins/tpm ]; then
+  # offer to remove it
+  read -p "❗ TPM is already installed. Do you want to remove it? (y/n) " response
+  if [[ $response =~ ^[Yy]$ ]]; then
+    rm -rf ~/.tmux/plugins/tpm
+  else
+    echo "🗨️ TPM is already installed. Skipping installaton."
+    exit 0
+  fi
+fi
+
 # install TPM
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
