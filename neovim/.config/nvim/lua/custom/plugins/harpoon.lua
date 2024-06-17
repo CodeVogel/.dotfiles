5f2bfb585e5bb512fae7c8a24d3e4b9a7b693979
Kamiel de Visser <kamieldevisser@gmail.com>
2024-06-17 14:48:29 +0200

    disable telescope for harpoon

--- neovim/.config/nvim/lua/custom/plugins/harpoon.lua
+++ neovim/.config/nvim/lua/custom/plugins/harpoon.lua
@@ -4,7 +4,6 @@ return {
       branch = 'harpoon2',
       dependencies = {
          'nvim-lua/plenary.nvim',
-         'nvim-telescope/telescope.nvim',
       },
       config = function()
          -- basic config from https://github.com/ThePrimeagen/harpoon/tree/harpoon2
@@ -17,9 +16,9 @@ return {
          vim.keymap.set('n', '<leader>a', function()
             harpoon:list():add()
          end)
-         vim.keymap.set('n', '<leader>hH', function()
+         vim.keymap.set('n', '<leader>hh', function()
             harpoon.ui:toggle_quick_menu(harpoon:list())
-         end, { desc = 'Open harpoon window (built-in)' })
+         end, { desc = 'Open harpoon window' })
 
          vim.keymap.set('n', '<C-h>', function()
             harpoon:list():select(1)
@@ -34,37 +33,13 @@ return {
             harpoon:list():select(4)
          end)
 
-         -- Toggle previous & next buffers stored within Harpoon list
-         vim.keymap.set('n', '<C-S-P>', function()
-            harpoon:list():prev()
-         end)
-         vim.keymap.set('n', '<C-S-N>', function()
-            harpoon:list():next()
-         end)
-
-         -- basic telescope configuration
-         local conf = require('telescope.config').values
-         local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-               table.insert(file_paths, item.value)
-            end
-
-            require('telescope.pickers')
-               .new({}, {
-                  prompt_title = 'Harpoon',
-                  finder = require('telescope.finders').new_table {
-                     results = file_paths,
-                  },
-                  previewer = conf.file_previewer {},
-                  sorter = conf.generic_sorter {},
-               })
-               :find()
-         end
-
-         vim.keymap.set('n', '<leader>hh', function()
-            toggle_telescope(harpoon:list())
-         end, { desc = 'Open harpoon window in Telescope' })
+         -- -- Toggle previous & next buffers stored within Harpoon list
+         -- vim.keymap.set('n', '<C-S-P>', function()
+         --    harpoon:list():prev()
+         -- end)
+         -- vim.keymap.set('n', '<C-S-N>', function()
+         --    harpoon:list():next()
+         -- end)
       end,
    },
    -- Indicator of current harpoon selection in Lualine --
